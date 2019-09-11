aed9b0a56b4823a0314b964ea0de119d15554544
ngolodets <nina.golodets@gmail.com>
2019-09-11 12:32:08 -0700

    working on server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(__dirname + '/client/build'));
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useFindAndModify: false});
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log(`💥💥💥 Connected to Mongo on ${db.host}:${db.port}`);
+});
+db.on('error', (err) => {
+  console.log(`Database error:\n${err}`);
+});
+
+app.use('/api', require('./routes/api'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`💥💥💥 ... listening on port ${process.env.PORT} ... 💥💥💥`)
+});
